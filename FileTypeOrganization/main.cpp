06f871ab994838b657f64af77aa854d36d25c7ef
seanMj <jesky.sean@protonmail.com>
2023-03-16 22:18:31 -0600

    various fixes to formatting and refactoring

--- FileTypeOrganization/main.cpp
+++ FileTypeOrganization/main.cpp
@@ -19,18 +19,18 @@ using std::string;
 #include <algorithm>
 #include  <fstream>
 using std::ofstream;
-/// <summary>
-/// get data from the selected directory
-/// </summary>
-/// <param name="directory"></param>
-/// <returns>vector</returns>
+#include <omp.h>
+#include <thread>
+
+/// @brief collect all files from the system to use as the data set for file organization
+/// @param directory 
+/// @return 
 const inline vector<fs::path> collect_relivant_files(const fs::path& directory)
 {
 
 	//save file names to files;
-	vector<fs::path> files;
-	std::vector<fs::path>* p_files = &files;
-	files.reserve(5000000);
+	vector<fs::path>* p_vfiles = new std::vector<fs::path>;
+	p_vfiles->reserve(5000000);
 	try {
 		for (const fs::directory_entry& dir : fs::recursive_directory_iterator{ directory })
 		{
@@ -42,7 +42,7 @@ const inline vector<fs::path> collect_relivant_files(const fs::path& directory)
 				{
 					if (file_path_p->has_extension())
 					{
-						p_files->push_back(*file_path_p);
+						p_vfiles->push_back(*file_path_p);
 					}
 				}
 			}
@@ -52,23 +52,24 @@ const inline vector<fs::path> collect_relivant_files(const fs::path& directory)
 		std::cout << "Filesys error" << "\n" << err.what() << std::endl;
 	}
 
-	std::sort(p_files->begin(), p_files->end());
+	std::sort(p_vfiles->begin(), p_vfiles->end());
 
+	std::vector<fs::path> files = *p_vfiles;
+	delete p_vfiles;
 	return files;
 }
-/// <summary>
-/// 
-/// </summary>
-/// <param name="collected_data"></param>
-/// <returns></returns>
-//get extensions based on file extensions within lookup directory
-//and push back one of each to vector to exclude duplicates:
+
+
+
+/// @brief get extensions based on file extensions within lookup directory and push back one of each to vector to exclude duplicates:
+/// @param collected_data 
+/// @return 
 const inline vector<fs::path> get_extensions(vector<fs::path>* collected_data)
 {
-	vector<string> extension_types;
-	vector<string>* p_types = &extension_types;
+	std::vector<std::string>* p_types = new std::vector<std::string>;
+	//vector<string>* p_types = &extension_types;
 	//to increasse the speed of the code, reserve the amount of space that will be needed by the code
-	extension_types.reserve(collected_data->size());
+	p_types->reserve(collected_data->size());
 	//go through the collected data(every file and directory)
 	for (const auto& current_filename : (*(collected_data)))
 	{
@@ -81,8 +82,8 @@ const inline vector<fs::path> get_extensions(vector<fs::path>* collected_data)
 		}
 	}
 	//create a vector to save the valid file extension data within
-	vector<fs::path> types_no_duplicates;
-	vector<fs::path>* p_types_no_duplicates = &types_no_duplicates;
+	vector<fs::path>* types_no_duplicates = new std::vector<fs::path>;
+	//vector<fs::path>* p_types_no_duplicates = &types_no_duplicates;
 	//sort the data by alpha. order.
 	std::sort(p_types->begin(), p_types->end());
 	//go through the sorted vector one by one:
@@ -92,27 +93,40 @@ const inline vector<fs::path> get_extensions(vector<fs::path>* collected_data)
 		if ((*(p_types))[i] != (*(p_types))[i - 1])
 		{
 			//then place it in the new vector, this is the list that will create the dir.list to copy to
-			p_types_no_duplicates->push_back((*(p_types))[i]);
+			types_no_duplicates->push_back((*(p_types))[i]);
 			//print the extensions that will become the directories to copy to
-			std::cout << (*(p_types))[i] << '\n';
+			std::cout << (*(p_types))[i] << '\t' << '\n';
 		}
 	}
+	delete p_types;
+	std::vector<fs::path> type_nd = *types_no_duplicates;
+	delete types_no_duplicates;
 	//send the valid file extension type data to vector for creating the directories
-	return types_no_duplicates;
+	return type_nd;
 }
 
-inline void create_directory_from_extension_type(const fs::path& base_save_location, const vector<fs::path>& extensions)
+
+/// @brief create a directory based off the extension type of files provided from a vector of pointers to fs::path extensions
+/// @param base_save_location 
+/// @param extensions 
+inline void create_directory_from_extension_type(const fs::path& base_save_location, const vector<fs::path>* extensions)
 {
 	//vector<fs::path> save_location;
-	for (auto& i : extensions)	//go through extension types presented from prev. function.
+	for (auto& i : (*(extensions)))	//go through extension types presented from prev. function.
 	{
 		fs::create_directory((base_save_location.string()) + i.string().erase(0,1));		//create dir based on extensions
 	}
 }
+
+
+/// @brief copy files from the computer's filesystem into a directory for the user
+/// @param dest 
+/// @param files_to_copy 
+/// @param extensions 
 void copy_relevent_files(const fs::path dest, vector<fs::path>* files_to_copy, vector<fs::path>* extensions)
 {
 	//call function to create the directorys needed to copy to
-	create_directory_from_extension_type(dest, *extensions);
+	create_directory_from_extension_type(dest, extensions);
 	for (const auto& ext : *extensions)
 	{
 		int i = 0;
@@ -123,26 +137,27 @@ void copy_relevent_files(const fs::path dest, vector<fs::path>* files_to_copy, v
 			{
 				if (files.extension() == ext)
 				{
+					//std::cout << "copying: " << files << '\t' << files.extension() << '\n';
+					const fs::path core_path = dest.string() + ext.string().erase(0,1);
 					try
 					{
 						//copy files to new location that are based on its extension
-						fs::copy(files, (dest.string() + ext.string().erase(0,1)), fs::copy_options::recursive | fs::copy_options::update_existing);
-						//create the filename destination and save location to be used by the functions below:
-						const fs::path core_path = dest.string() + ext.string();
-						const fs::path save_this = core_path.string() + "\\" + files.filename().string();
-						const fs::path save_at = core_path.string() + "\\" + files.filename().string() + "_" + std::to_string(++(*(p_i))) + "_" + std::to_string(time(NULL)) + files.extension().string();
-						//rename all the files in their new location....after they have been copied, this should be rewritten with Multi-threads.
-						if (fs::exists(save_this))
-						{
-							fs::rename(save_this, save_at.string());
-						}
-					}
+						fs::copy(files, (core_path.string()), fs::copy_options::recursive | fs::copy_options::update_existing);
+	}
 					//if an error occurs, print error in log file for later lookup if needed by user.
 					catch (std::runtime_error& err)
 					{
 						ofstream logs_(dest.string() + "error.txt", std::ios::app);
 						logs_ << err.what() << "\n" << dest.string() + ext.string() << '\n';
 
+					}
+						//create the filename destination and save location to be used by the functions below:
+					const fs::path save_this = core_path.string() + "\\" + files.filename().string();
+					const fs::path save_at = save_this.string() + "_" + std::to_string(++(*(p_i))) + "_" + std::to_string(time(NULL)) + files.extension().string();
+					//rename all the files in their new location....after they have been copied, this should be rewritten with Multi-threads.
+					if (fs::exists(save_this))
+					{
+						fs::rename(save_this, save_at);
 					}
 				}
 			}    //comp = false;
@@ -156,7 +171,7 @@ int main(int argv, char* argc[])
 	//im doing IO, so every bit, every second is relivant in this case.. which;
 	//i didnt add them until after i examined the output performance
 
-	std::string copy_from = argc[1];
+	const std::string copy_from = argc[1];
 	const fs::path path_to_copy_from = copy_from;
 
 	//const fs::path path_to_copy_from = "";
@@ -168,17 +183,19 @@ int main(int argv, char* argc[])
 	//vector<fs::path>* p_extn = &extn;
 	*p_extn = get_extensions(p_files);
 
-	std::string save_location = argc[2];
+	const std::string save_location = argc[2];
 	const fs::path base_save_location = save_location;
 
 	std::cout << "\n\nThere are:\t" << p_files->size() << " Files To Be Copied...\n";
 	std::cout << "\n\nThere are:\t" << p_extn->size() << " Extensions Directories To Be Created...\n";
 
-	if (p_files->size() >= 1000) {
-		std::cout << "\n\nPlease Be Patient, There Are >= 1000 Files, Else, Press Ctrl + C To Quit Program.\n";
+	if (p_files->size() >= 10'000) {
+		std::cout << "\n\nPlease Be Patient...\n";
 	}
-	copy_relevent_files(base_save_location, p_files, p_extn);
-
+	 //#pragma omp parallel
+		copy_relevent_files(base_save_location, p_files, p_extn);
+	//#pragma omp barrier
+	
 	delete p_files;
 	delete p_extn;
 	
